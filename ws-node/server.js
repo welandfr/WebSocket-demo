0c7e210dc08971bae5c26c5d2425c419c9bd7734
Fredrik Welander <fredrik.welander@subsite.fi>
2023-05-20 20:20:01 +0300

    Initial commit

---
+++ ws-node/server.js
@@ -0,0 +1,36 @@
+const WebSocket = require('ws')
+const os = require('os')
+require('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+const wss = new WebSocket.Server({ port: PORT });
+
+wss.on('connection', (ws, req) => {
+    console.log('Client connected');
+
+    const urlParams = new URLSearchParams(req.url.slice(1));
+    if (urlParams.get('token') !== process.env.TOKEN) {
+        console.log('Invalid token: ' + urlParams.get('token'));
+        ws.send(JSON.stringify({
+            status: 1,
+            msg: 'ERROR: Invalid token.'
+        }));
+        ws.close();
+    }
+
+    ws.on('message', (message) => {
+        console.log('Received message:', message);
+
+        // Send a response back to the client along with some other info
+        ws.send(JSON.stringify({
+            status: 0,
+            msg: String(message).toUpperCase(),
+            freemem: Math.round(os.freemem() / 1024 / 1024), // MB
+            totalmem: Math.round(os.totalmem() / 1024 / 1024) // MB
+        }));
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
